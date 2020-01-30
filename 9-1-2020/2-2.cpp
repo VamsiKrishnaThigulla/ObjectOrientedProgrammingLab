5f14e9585f20c8b7f24387a8a1510438686ec96a
thesrsakabuvttchi <58292377+thesrsakabuvttchi@users.noreply.github.com>
2020-01-30 14:08:14 +0530

    First -1

---
+++ 9-1-2020/2-2.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define SIZE 10000
+
+int position(int arr[],int size, int val,int start)
+{
+  int jmp=2,tmp;                           //jump value for quadratic probing
+  if(arr[0]==-1)
+	return(0);
+  for(int p=0;pow(2,p)<size;p++)
+    {   tmp=(int)pow(2,p)+start;
+        if(arr[tmp]==val&&arr[tmp-1]!=val)
+        	return((int)pow(2,p)+start);
+    	if(arr[tmp]==val)   	 
+    	 	return(position(arr,size,val,(int)pow(2,p-1)));
+	}
+}
+
+int main()
+{
+	int arr[SIZE];
+	int i=0,mrk=0;
+	cout<<"Enter the members of the array"<<endl;
+	for(i=0;i<SIZE;i++)
+	{	
+		if(mrk==1)
+			arr[i]=-1;
+		else
+			cin>>arr[i];
+		
+		if(arr[i]==-1)
+			mrk=1;
+	}
+	cout<<"First -1 is found at "<<position(arr,SIZE,-1,0)+1<<" position"<<endl;
+}
